c3d9fed789152d25c168ddc14095122199b396b6
Markus Appel <markusappel@hotmail.de>
2021-05-19 23:40:03 +0200

    Work in progress

--- lib/pathfinder-infra-stack.ts
+++ lib/pathfinder-infra-stack.ts
@@ -11,23 +11,30 @@ export class PathfinderInfraStack extends cdk.Stack {
     // A VPC that contains all infrastructure
     const vpc = new ec2.Vpc(this, 'MainVpc', {});
 
+    const dbAdminName = 'dbAdmin';
+    const dbName = 'teamspeak'
+
     // The MySQL Database for Teamspeak
     const dbSecret = new secretsmanager.Secret(this, 'TeamspeakDbSecret', {
       generateSecretString: {
-        secretStringTemplate: JSON.stringify({ username: 'user' }),
+        secretStringTemplate: JSON.stringify({ username: dbAdminName }),
         generateStringKey: 'password',
       },
     });
 
-    const dbCrendentials = rds.Credentials.fromSecret(dbSecret, "dbadmin");
-    const dbCluster = new rds.ServerlessCluster(this, 'TeamspeakDbCluster', {
+    const dbCrendentials = rds.Credentials.fromSecret(dbSecret, dbAdminName);
+    const dbInstance = new rds.DatabaseInstance(this, 'TeamspeakDb', {
+      engine: rds.DatabaseInstanceEngine.mariaDb({
+        version: rds.MariaDbEngineVersion.VER_10_5_8
+      }),
       vpc: vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE,
-      },
-      engine: rds.DatabaseClusterEngine.AURORA_MYSQL,
-      credentials: dbCrendentials,
-    });
+      databaseName: dbName,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      credentials: dbCrendentials
+    })
 
     // The ECS cluster (only used with Fargate tasks)
     const cluster = new ecs.Cluster(this, 'Cluster', {
@@ -35,29 +42,36 @@ export class PathfinderInfraStack extends cdk.Stack {
     });
 
     // The Teamspeak Fargate task
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Teamspeak');
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Teamspeak', {
+      cpu: 512,
+      memoryLimitMiB: 2048
+    });
 
     taskDefinition.addContainer('TeamspeakContainer', {
       image: ecs.ContainerImage.fromRegistry("teamspeak"),
       memoryLimitMiB: 512,
       environment: {
-        TS3SERVER_DB_PLUGIN: "ts3db_mysql",
-        TS3SERVER_DB_SQLCREATEPATH: "create_mysql",
-        TS3SERVER_DB_HOST: dbCluster.clusterEndpoint.hostname,
+        TS3SERVER_DB_PLUGIN: "ts3db_mariadb",
+        TS3SERVER_DB_SQLCREATEPATH: "create_mariadb",
+        TS3SERVER_DB_HOST: dbInstance.dbInstanceEndpointAddress,
         TS3SERVER_DB_USER: dbCrendentials.username,
         TS3SERVER_DB_PASSWORD: "${DB_SECRET}",
-        TS3SERVER_DB_NAME: "teamspeak",
+        TS3SERVER_DB_NAME: dbName,
         TS3SERVER_DB_WAITUNTILREADY: "30",
         TS3SERVER_LICENSE: "accept"
       },
       secrets: {
         DB_SECRET: ecs.Secret.fromSecretsManager(dbSecret)
-      }
+      },
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: "teamspeak-task-logs"
+      })
     });
 
-    const ecsService = new ecs.FargateService(this, 'MainCluster', {
+    const ecsService = new ecs.FargateService(this, 'TeamspeakService', {
       taskDefinition,
-      cluster
+      cluster,
+      assignPublicIp: true
     })
   }
 }
