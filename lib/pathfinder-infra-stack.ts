b06667f71b64943883454965dba327e94b09fc3f
Markus Appel <markusappel@hotmail.de>
2021-05-21 01:34:35 +0200

    Use Loadbalancer

--- lib/pathfinder-infra-stack.ts
+++ lib/pathfinder-infra-stack.ts
@@ -1,113 +1,180 @@
-import * as cdk from '@aws-cdk/core';
-import * as ecs from '@aws-cdk/aws-ecs';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as rds from '@aws-cdk/aws-rds';
-import * as iam from '@aws-cdk/aws-iam';
-import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
+import * as cdk from "@aws-cdk/core";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as rds from "@aws-cdk/aws-rds";
+import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
+import * as lb from "@aws-cdk/aws-elasticloadbalancingv2";
 
 export class PathfinderInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'MainVpc', {});
+    const teamspeakDbName = "teamspeak";
+    const teamspeakDbUser = "tsuser";
 
-    // ### Security Groups ### //
+    const teamspeakDbInstanceType = ec2.InstanceType.of(
+      ec2.InstanceClass.T2,
+      ec2.InstanceSize.MICRO
+    );
 
-    const teamspeakSecurityGroup = new ec2.SecurityGroup(this, 'TeamspeakSg', {
-      vpc: vpc,
-      allowAllOutbound: true
-    })
+    const UDP = {
+      ecsProtocol: ecs.Protocol.UDP,
+      lbProtocol: lb.Protocol.UDP,
+      ec2Protocol: ec2.Protocol.UDP,
+    };
 
-    const dbSecurityGroup = new ec2.SecurityGroup(this, 'TeamspeakDbSg', {
-      vpc: vpc,
-      allowAllOutbound: true
-    })
+    const TCP = {
+      ecsProtocol: ecs.Protocol.TCP,
+      lbProtocol: lb.Protocol.TCP,
+      ec2Protocol: ec2.Protocol.TCP,
+    };
 
-    dbSecurityGroup.addIngressRule(teamspeakSecurityGroup, ec2.Port.tcp(3306), "Access of DB from Teamspeak")
-
-    teamspeakSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(10011), "Access for clients of TeamSpeak #1")
-    teamspeakSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(30033), "Access for clients of TeamSpeak #2")
-    teamspeakSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(9987), "Access for clients of TeamSpeak #3")
-
-    // ### Teamspeak Database ###
-
-    const dbAdminName = 'dbAdmin'
-    const dbName = 'teamspeak'
-
-    const dbPassword = new secretsmanager.Secret(this, 'TeamspeakDbPassword', {
-      generateSecretString: {
-        includeSpace: false,
-        excludePunctuation: true
+    const teamspeakPorts = [
+      {
+        port: 10011,
+        protocol: TCP,
       },
-    })
+      {
+        port: 30033,
+        protocol: TCP,
+      },
+      {
+        port: 9987,
+        protocol: UDP,
+      },
+    ];
 
-    const dbCrendentials = rds.Credentials.fromPassword(dbAdminName, dbPassword.secretValue)
+    const vpc = new ec2.Vpc(this, "MainVpc");
 
-    const dbInstance = new rds.DatabaseInstance(this, 'TeamspeakDb', {
+    const teamspeakSecurityGroup = new ec2.SecurityGroup(this, "TeamspeakSg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+    });
+
+    const teamspeakDbSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "TeamspeakDbSg",
+      {
+        vpc: vpc,
+        allowAllOutbound: true,
+      }
+    );
+
+    teamspeakDbSecurityGroup.addIngressRule(
+      teamspeakSecurityGroup,
+      ec2.Port.tcp(3306),
+      "Access of DB from Teamspeak"
+    );
+
+    const loadBalancer = new lb.NetworkLoadBalancer(this, "LoadBalancer", {
+      vpc: vpc,
+      internetFacing: true,
+    });
+
+    teamspeakPorts.forEach((forwarding) => {
+      teamspeakSecurityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        new ec2.Port({
+          protocol: forwarding.protocol.ec2Protocol,
+          fromPort: forwarding.port,
+          toPort: forwarding.port,
+          stringRepresentation: `${forwarding.port}@${forwarding.protocol.ec2Protocol}`,
+        }),
+        "Access for clients of TeamSpeak"
+      );
+    });
+
+    const teamspeakDbPassword = new secretsmanager.Secret(
+      this,
+      "TeamspeakDbPassword",
+      {
+        generateSecretString: {
+          includeSpace: false,
+          excludePunctuation: true,
+        },
+      }
+    );
+
+    const teamspeakDbCredentials = rds.Credentials.fromPassword(
+      teamspeakDbUser,
+      teamspeakDbPassword.secretValue
+    );
+
+    const teamspeakDbInstance = new rds.DatabaseInstance(this, "TeamspeakDb", {
       engine: rds.DatabaseInstanceEngine.mariaDb({
-        version: rds.MariaDbEngineVersion.VER_10_5_8
+        version: rds.MariaDbEngineVersion.VER_10_5_8,
       }),
       vpc: vpc,
-      databaseName: dbName,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO
-      ),
-      credentials: dbCrendentials,
-      securityGroups: [dbSecurityGroup]
-    })
+      databaseName: teamspeakDbName,
+      instanceType: teamspeakDbInstanceType,
+      credentials: teamspeakDbCredentials,
+      securityGroups: [teamspeakDbSecurityGroup],
+    });
 
-    // ### Compute Cluster ###
+    const mainCluster = new ecs.Cluster(this, "MainCluster", {
+      vpc: vpc,
+    });
 
-    const cluster = new ecs.Cluster(this, 'Cluster', {
-      vpc: vpc
-    })
-
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Teamspeak', {
+    const taskDefinition = new ecs.FargateTaskDefinition(this, "Teamspeak", {
       cpu: 512,
-      memoryLimitMiB: 2048
-    })
+      memoryLimitMiB: 2048,
+    });
 
-    taskDefinition.addContainer('TeamspeakContainer', {
+    taskDefinition.addContainer("TeamspeakContainer", {
       image: ecs.ContainerImage.fromRegistry("teamspeak"),
       memoryLimitMiB: 512,
       environment: {
         TS3SERVER_DB_PLUGIN: "ts3db_mariadb",
         TS3SERVER_DB_SQLCREATEPATH: "create_mariadb",
-        TS3SERVER_DB_HOST: dbInstance.dbInstanceEndpointAddress,
-        TS3SERVER_DB_USER: dbCrendentials.username,
-        TS3SERVER_DB_NAME: dbName,
+        TS3SERVER_DB_HOST: teamspeakDbInstance.dbInstanceEndpointAddress,
+        TS3SERVER_DB_USER: teamspeakDbCredentials.username,
+        TS3SERVER_DB_NAME: teamspeakDbName,
         TS3SERVER_DB_WAITUNTILREADY: "30",
-        TS3SERVER_LICENSE: "accept"
-      }, 
+        TS3SERVER_LICENSE: "accept",
+      },
       secrets: {
-        TS3SERVER_DB_PASSWORD: ecs.Secret.fromSecretsManager(dbPassword)
+        TS3SERVER_DB_PASSWORD:
+          ecs.Secret.fromSecretsManager(teamspeakDbPassword),
       },
       logging: ecs.LogDriver.awsLogs({
-        streamPrefix: "teamspeak-task-logs"
+        streamPrefix: "teamspeak-task-logs",
       }),
-      portMappings: [
-        {
-          containerPort: 9987,
-          hostPort: 9987,
-          protocol: ecs.Protocol.UDP
-        },
-        {
-          containerPort: 10011,
-          hostPort: 10011
-        },
-        {
-          containerPort: 30033,
-          hostPort: 30033
-        }
-      ]
-    })
+      portMappings: teamspeakPorts.map((element) => {
+        return {
+          containerPort: element.port,
+          hostPort: element.port,
+          protocol: element.protocol.ecsProtocol,
+        };
+      }),
+    });
 
-    const ecsService = new ecs.FargateService(this, 'TeamspeakService', {
+    const teamspeakService = new ecs.FargateService(this, "TeamspeakService", {
       taskDefinition,
-      cluster,
-      assignPublicIp: true,
-      securityGroups: [teamspeakSecurityGroup]
-    })
+      cluster: mainCluster,
+      securityGroups: [teamspeakSecurityGroup],
+    });
+
+    teamspeakPorts.forEach((forwarding) => {
+      const listener = loadBalancer.addListener(`Listener${forwarding.port}`, {
+        port: forwarding.port,
+        protocol: forwarding.protocol.lbProtocol,
+      });
+
+      listener.addTargets(`AddTargetGroupsId${forwarding.port}`, {
+        port: forwarding.port,
+        protocol: forwarding.protocol.lbProtocol,
+        targets: [
+          teamspeakService.loadBalancerTarget({
+            containerName: "TeamspeakContainer",
+            containerPort: forwarding.port,
+            protocol: forwarding.protocol.ecsProtocol, // Not actually used - still fails without it
+          }),
+        ],
+        healthCheck: {
+          protocol: lb.Protocol.TCP,
+          port: "10011",
+        },
+      });
+    });
   }
 }
--- test/pathfinder-infra.test.ts
+++ test/pathfinder-infra.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as PathfinderInfra from '../lib/pathfinder-infra-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as PathfinderInfra from "../lib/pathfinder-infra-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new PathfinderInfra.PathfinderInfraStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new PathfinderInfra.PathfinderInfraStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
